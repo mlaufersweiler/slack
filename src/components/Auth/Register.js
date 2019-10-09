eb079f454220fb77a80b65d8d30f34578cb47add
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-10-09 11:21:02 -0400

    user able to register with firebase

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,8 +1,103 @@
 import React, { Component } from "react";
+import firebase from "../../firebase";
+import { Link } from "react-router-dom";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
 
 class Register extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConformation: ""
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(createdUser => {
+        console.log(createdUser);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
   render() {
-    return <div>Register</div>;
+    const { username, email, password, passwordConformation } = this.state;
+
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for DevChat
+          </Header>
+          <Form onSubmit={this.handleSubmit} size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                value={username}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.handleChange}
+                value={email}
+                type="email"
+              />
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+                value={password}
+                type="password"
+              />
+              <Form.Input
+                fluid
+                name="passwordConformation"
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Password Conformation"
+                onChange={this.handleChange}
+                value={passwordConformation}
+                type="password"
+              />
+              <Button color="orange" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already a user? <Link to="/login">Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
 
 const Root = () => (
   <Router>
