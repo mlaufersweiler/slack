cc23c4a1a8e66e18b9128e7e720c73fc223026c5
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-10-14 14:16:40 -0400

    working on sidepanel component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
+import { Grid } from "semantic-ui-react";
 import "./App.css";
+import { connect } from "react-redux";
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
 
-class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = ({ currentUser }) => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel currentUser={currentUser} />
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
 
-export default App;
+export default connect(mapStateToProps)(App);
---
+++ src/components/ColorPanel/ColorPanel.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class ColorPanel extends Component {
+  render() {
+    return <div>ColorPanel</div>;
+  }
+}
+export default ColorPanel;
